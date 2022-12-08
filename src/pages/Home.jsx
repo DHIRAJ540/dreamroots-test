70f659d309b6ff90ed06e04c6d8837df6e68f000
dhiraj540 <dhirajsubudhi540@example.com>
2022-12-08 23:15:45 +0530

    image type changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           <div className="flex items-center justify-center p-2 rounded-[30px] w-[98px] h-[98px] custom_shadow">
             <img src={headerImg} alt="header" />
           </div>
-          <h1 className="text-[32px] font-[800] custom_gradient ml-[25px]">
+          <h1 className="text-[32px] font-[800] custom_gradient ml-[25px]  ">
             Dental Crown
           </h1>
         </div>
@@ -33,17 +33,21 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-[25px] flex items-center justify-between px-[25px]">
-        <img src={sampleImg} alt="sample" />
+        <img
+          src={sampleImg}
+          alt="sample"
+          className="w-[179px] h-[145px] rounded-[30px] "
+        />
         <div className="ml-[20px]">
           <h2 className="text-[20px] font-bold text-primary leading-[18px]">
             Why would I need a dental crown?
           </h2>
-          <p className="mt-[10px] text-primary text-[15px] leading-[17px] ">
+          <p className="mt-[10px] text-primary text-[15px] leading-[17px] font-[500] ">
             You may need a dental crown for several reasons, including:
           </p>
         </div>
       </div>
-      <div className="mt-[25px] mx-[25px] px-[35px] py-[20px] text-primary bg-[#E7C985] rounded-[30px] text-[13px] leading-[15px] font-normal ">
+      <div className="mt-[25px] mx-[25px] px-[35px] py-[20px] text-primary bg-[#E7C985] rounded-[30px] text-[13px]  leading-[15px] font-normal ">
         <ul className="list-disc">
           <li>
             Protecting a weak tooth (possibly from decay) from breaking or to
